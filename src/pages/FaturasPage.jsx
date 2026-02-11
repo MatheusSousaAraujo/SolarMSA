03d6ac902fe60f1d683f097d07207b3c22783aaa
MatheusSousaAraujo <165159879+MatheusSousaAraujo@users.noreply.github.com>
2026-02-11 19:27:23 +0000

    Deploying to gh-pages from @ MatheusSousaAraujo/SolarMSA@b83e89c9e60d675647dfe622f97d092ccc48105d 🚀

--- src/pages/FaturasPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function FaturasPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold text-white">Gestão de Faturas</h1>
-      <p className="text-gray-400 mt-2">Aqui você poderá ver, adicionar e gerenciar todas as faturas.</p>
-    </div>
-  );
-}
