775d498f21d59fe045e7ecec826776531f0d1695
MatheusSousaAraujo <165159879+MatheusSousaAraujo@users.noreply.github.com>
2026-02-11 19:18:54 +0000

    Deploying to gh-pages from @ MatheusSousaAraujo/SolarMSA@3bb35cad90bf31d193e37eab04170d520ed70382 🚀

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: '/SolarMSA/', // Isso garante que o Vite procure os arquivos em /SolarMSA/ e não na raiz /
 })
